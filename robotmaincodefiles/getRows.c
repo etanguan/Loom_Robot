7fdf68aed719455d54639ad67c6c301c5a96fa0f
arudolph28 <150281807+arudolph28@users.noreply.github.com>
2023-11-17 11:08:11 -0500

    Update getRows.c

--- robotmaincodefiles/getRows.c
+++ robotmaincodefiles/getRows.c
@@ -2,8 +2,8 @@
 
 int getRows()
 {
-	const int MIN_ROWS = 1;
-	const int MAX_ROWS = 10;
+	const int MIN_ROWS = 2;
+	const int MAX_ROWS = 20;
 	const int INIT_ROWS = 5;
 	displayString(3, "Select number of rows");
 	int rows = INIT_ROWS;
@@ -18,7 +18,7 @@ int getRows()
 			{	}
 			while( getButtonPress(UP_BUTTON) )
 			{	}
-			rows += 1;
+			rows += 2;
 		}
 
 		if( getButtonPress(DOWN_BUTTON) )
@@ -27,7 +27,7 @@ int getRows()
 			{	}
 			while( getButtonPress(DOWN_BUTTON) )
 			{	}
-			rows -= 1;
+			rows -= 2;
 		}
 
 		if( rows<MIN_ROWS || rows>MAX_ROWS )
